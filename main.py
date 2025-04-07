3f310d32d42c3d30c91ec915bbc28f1aa13074a8
sokviseth-bori <2024302bori@aupp.edu.kh>
2025-04-07 13:03:20 +0700

    Revert "Reapply "initial commit""
    This reverts commit df3e69850a64a7389d7b520f51b8df87e2c2fa0c.

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
 print("hello world")
-
-for i in range(10): 
-    print("hello world") 
-
-def add(a,b):
-    return a +b #green bar on the left means it is added to the git repo, orange means u changed it but not added to the repo
